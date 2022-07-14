4528b1f36e7baa74e55d186d4bbea41c94fc82ad
Farhinarius <farhat3027@gmail.com>
2022-07-14 16:29:33 +0300

    update file structure

---
+++ learn/basics/dynamic_collections.py
@@ -0,0 +1,21 @@
+# creates a empty list
+nums = []
+
+# appending data in list
+nums.append(21)
+nums.append(40.5)
+nums.append("String")
+
+print(nums)
+
+# Python program to illustrate a Dictionary
+
+# creates a empty list
+Dict = []
+
+# putting integer values
+Dict = {"A": 'Geeks', "B": 'For', "C": 'Geeks'}
+
+print(Dict)
+
+#Code submitted by Susobhan AKhuli
---
+++ learn/basics/variables.py
@@ -0,0 +1,7 @@
+my_number = 3
+print(my_number)
+
+my_number = "somestring"
+print(my_number)
+
+print(my_number)
